ac8617031c8068d237023785f560b8d4cc0f6152
qianzhiyao26@126.com <qianzhiyao26@126.com>
2019-03-27 15:01:47 +0000

    python code for experiments

--- DOI_webofscience.py
+++ DOI_webofscience.py
@@ -4,13 +4,20 @@ extract DOI from Web of Science file
 
 import re
  
-file_object = open('E:\Download\savedrecs (1).txt','rU', encoding='UTF-8')
+file_object = open('E:\Download\savedrecs (7).ciw','rU', encoding='UTF-8')
 f = open('out.txt','w', encoding='UTF-8')
+i=0
 try:
     for line in file_object:
-        g = re.search("(?<=DI )10\.([0-9]{4}).*", line)
+#        g = re.search("(?<=DI )10\.([0-9]{4}).*", line)
+        if re.search("(?<=TC )[0-9]{3,5}", line):
+            i =i+1
+        g = re.search("(?<=TC )[0-9]{3,5}", line)
+        
         if g:
             print(g.group())
-            f.writelines(line)
+            f.writelines(g.group()+'/n')
 finally:
      file_object.close()
+     
+print(i)
---
+++ wos_use.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+use wos to find the citation for highly-cited paper
+"""
+import wos
+
+#with wos.WosClient('s1815492@ed.ac.uk', 'q499912042zy`') as client:
+#    print(wos.utils.query(client, 'AU=Knuth Donald'))
+dir(wos)
