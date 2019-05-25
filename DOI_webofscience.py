511f799ab479ccd75bb40350d2ac59ec748d7bd3
qianzhiyao26@126.com <qianzhiyao26@126.com>
2019-05-25 17:05:51 +0100

    add origin data
    citations on plos one of top 10 highly-cited paper

--- DOI_webofscience.py
+++ DOI_webofscience.py
@@ -3,21 +3,96 @@ extract DOI from Web of Science file
 '''
 
 import re
- 
-file_object = open('E:\Download\savedrecs.ciw','rU', encoding='UTF-8')
-f = open('out.txt','w', encoding='UTF-8')
-#i=0
-try:
-    for line in file_object:
-        g = re.search("(?<=DI )10\.([0-9]{4}).*", line)
-#        get the times cited from search result
-#        if re.search("(?<=TC )[0-9]{3,5}", line):
-#            i =i+1
-#        g = re.search("(?<=TC )[0-9]{3,5}", line)        
-        if g:
-            print(g.group())
-            f.writelines(g.group()+'/n')
-finally:
-     file_object.close()
-     
-#print(i)
+import os
+import time
+from urllib.request import urlopen,Request
+from urllib.error import URLError
+
+def DOI_find(a):
+    l = []
+    file_object = open('savedrecs ('+str(a)+').ciw','rU', encoding='UTF-8')
+#        name = 'out('+str(a)+').txt'
+#        f = open(name,'w', encoding='UTF-8')
+    i=0
+    try:
+        for line in file_object:
+            g = re.search("(?<=DI )10\.([0-9]{4}).*", line)
+    #        get the times cited from search result
+    #        if re.search("(?<=TC )[0-9]{3,5}", line):
+    #            i =i+1
+    #        g = re.search("(?<=TC )[0-9]{3,5}", line)        
+            if g:
+#                print(g.group())
+                l.append(g.group())
+#                    f.writelines(g.group()+'\n')
+                i=i+1
+    finally:
+         file_object.close()
+         print(i,'            ',a)  
+    return l
+
+
+def PLOS_get(l,a):
+    i = 0    
+    for ids in l:
+        try:
+            url = "http://journals.plos.org/plosone/article/file?id="+ids+"&type=manuscript"
+            i=i+1
+            req = Request(url)
+            req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36')
+            html = urlopen(req)
+            name=str(a)+'\\'+str(i)+".xml"
+            folder = os.path.exists(str(a))
+            if not folder:      
+                os.makedirs(str(a))
+            with open(name, 'wb') as f:
+                f.write(html.read())
+                print(ids,"               ",name)
+                time.sleep(1)
+        except URLError as e:
+            with open("error.log",'a') as f:
+                f.write(str(a)+"/"+str(i)+"    "+str(e.code)+"    "+str(ids)+'\n')
+            print("error:"+str(a)+"\\"+str(i)+"    "+str(e.code)+"    "+str(ids))
+            pass
+        
+def PLOS_revise(ids,a,i):
+    try:
+        url = "http://journals.plos.org/plosone/article/file?id="+ids+"&type=manuscript"
+        req = Request(url)
+        req.add_header('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36')
+        html = urlopen(req)
+        name=str(a)+'\\'+str(i)+".xml"
+        folder = os.path.exists(str(a))
+        if not folder:      
+            os.makedirs(str(a))
+        with open(name, 'wb') as f:
+            f.write(html.read())
+            print(ids,"               ",name)
+            time.sleep(1)
+    except URLError as e:
+        with open("error.log",'a') as f:
+            f.write(str(a)+"/"+str(i)+"    "+str(e.code)+"    "+str(ids)+'\n')
+        print("error:"+str(a)+"\\"+str(i)+"    "+str(e.code)+"    "+str(ids))
+        pass
+    
+#for a in range(1,11):
+#    l = DOI_find(a)
+#    PLOS_get(l,a)
+#l = DOI_find(2)
+#PLOS_get(l,2)
+PLOS_revise("10.1371/journal.pone.0186461",9,12)
+PLOS_revise("10.1371/journal.pone.0186943",1,113)
+PLOS_revise("10.1371/journal.pone.0176993",1,149)
+PLOS_revise("10.1371/journal.pone.0197599",2,6)
+
+
+
+
+
+
+
+
+
+
+
+
--- plos_use.py
+++ plos_use.py
@@ -2,12 +2,52 @@
 use plos to get the paper
 '''
 
+#import requests
+#
+#def getPage(ids,API_KEY):
+#    baseurl = "http://alm.plos.org/api/v5/articles" 
+#    reqparam = { 'ids' : '10.1371%s2Fjournal.pone.%s'%("%",ids), 'info' : 'detail' ,'api_key' : API_KEY}
+#    r = requests.get(baseurl, params = reqparam)
+#    return r.text
+#
+#f = open('out1.txt','w', encoding='UTF-8')
+#f.write(getPage("0001543","9yaXEsAZxuBfUX_i42Wg"))
+#f.close()
+
 import requests
+import sys
+import json
+import pickle
+import re
+import time
+import numpy as np
 
 def getPage(ids,API_KEY):
-    baseurl = "http://api.plos.org/search?q=reference:'non+Watson+Crick'&fl=id" 
-    param = 'ids:' + '10.1371%s2Fjournal.pone.%s'%("%",ids) + '&api_key:' + API_KEY
-    r = requests.get(baseurl)
+    baseurl = " http://api.plos.org/search" 
+    reqparam = { 'ids' : '10.1371%s2Fjournal.pone.%s'%("%",ids), 'type' : 'manuscript' ,'api_key' : API_KEY}
+    r = requests.get(baseurl, params = reqparam)
     return r.text
 
-print(getPage("0001543","9yaXEsAZxuBfUX_i42Wg"))
+def flatten_json(y):
+    """
+    hacked to accept unicode
+    """
+    out = {}
+ 
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_') 
+                i += 1
+        else:
+            out[str(name[:-1])] = str(x)
+ 
+    flatten(y)
+    return out
+
+
+print(flatten_json(getPage("0001543","9yaXEsAZxuBfUX_i42Wg")))
